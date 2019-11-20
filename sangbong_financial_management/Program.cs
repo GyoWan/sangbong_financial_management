ce014d04f7f39eb944948354225ecdfdadbc64a0
GyoWan <ghkdrydhsk@nate.com>
2019-11-20 21:34:30 +0900

    ignore setting 재설정

--- sangbong_financial_management/Program.cs
+++ sangbong_financial_management/Program.cs
@@ -17,6 +17,7 @@ namespace sangbong_financial_management
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
