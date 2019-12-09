34e43c25f39b0d62d59c0d81e584ee142337ff10
GyoWan <ghkdrydhsk@nate.com>
2019-12-10 01:56:19 +0900

    Entity FrameWork Install

--- sangbong_financial_management/Program.cs
+++ sangbong_financial_management/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using sangbong_financial_management.SFM.Main;
+using System.Data.Entity;
 
 namespace sangbong_financial_management
 {
@@ -16,8 +18,9 @@ namespace sangbong_financial_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
 
+           
         }
     }
 }
